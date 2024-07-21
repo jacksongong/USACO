b61ce6b5cc3c9c97cb80dee3794fea59ccbf9f7f
jacksongong <166082474+jacksongong@users.noreply.github.com>
2024-07-21 18:36:33 -0400

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,8 @@
+#include <iostream>  // This is the header file for input and output
+
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;  // Print "Hello, World!" to the console
+    return 0;  // Indicate that the program ended successfully
+}
